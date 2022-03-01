77b23afe09a97a3c503e80e700060ef9841ac9b0
Pakorn <Pakorn.Thanaprachanon@outlook.com>
2022-03-02 00:37:39 +0700

    Fix invalid code

--- src/main/scala/com/travist/spark/test.scala
+++ src/main/scala/com/travist/spark/test.scala
@@ -9,7 +9,7 @@ object test {
     val sc = new SparkContext("local[*]", "HelloWorld")
     val lines = sc.textFile("data/ml-100k/u.data")
     val numLines = lines.count()
-    println("Hello world! T
+    println("Hello world! The u.data file has " + numLines + " lines.")
     sc.stop()
   }
 }
