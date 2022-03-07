48d4e45e7203c970b64fa77ae026fb094b814da5
Pakorn <Pakorn.Thanaprachanon@outlook.com>
2022-03-07 21:08:00 +0700

    Rearrange code cont

--- src/main/scala/com/travist/learn/HelloWorld.scala
+++ src/main/scala/com/travist/learn/HelloWorld.scala
@@ -1,3 +1,15 @@
-package com.travist.learn object HelloWorld {
+package com.travist.learn
 
+import org.apache.spark.sql.SparkSession
+
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession.builder.master("spark://localhost:7077").appName("SparkByExample").getOrCreate
+
+    println("First SparkContext:")
+    println("APP Name :" + spark.sparkContext.appName)
+    println("Deploy Mode :" + spark.sparkContext.deployMode)
+    println("Master :" + spark.sparkContext.master)
+  }
 }
