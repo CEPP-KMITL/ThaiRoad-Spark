a55beb887c86e63d8dbd98095b9fcfdbb4af004d
Pakorn <Pakorn.Thanaprachanon@outlook.com>
2022-03-08 22:44:32 +0700

    Add SparkSQL

--- src/main/scala/com/travist/learn/HelloWorld.scala
+++ src/main/scala/com/travist/learn/HelloWorld.scala
@@ -5,11 +5,13 @@ import org.apache.spark.sql.SparkSession
 object HelloWorld {
   def main(args: Array[String]): Unit = {
 
-    val spark = SparkSession.builder.master("spark://localhost:7077").appName("SparkByExample").getOrCreate
+    val spark = SparkSession.builder.master("local[*]").appName("SparkByExample").getOrCreate
 
     println("First SparkContext:")
     println("APP Name :" + spark.sparkContext.appName)
     println("Deploy Mode :" + spark.sparkContext.deployMode)
     println("Master :" + spark.sparkContext.master)
+
+    spark.stop()
   }
 }
